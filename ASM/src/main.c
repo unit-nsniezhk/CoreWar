cf2b33b7c8fc8a756aaa5edcfe87e3d25ffc8013
Stanislav BUDILKO <sbudilko@e2r10p15.unit.ua>
2019-03-04 17:32:33 +0200

    start writing main

--- ASM/src/main.c
+++ ASM/src/main.c
@@ -12,8 +12,34 @@
 
 #include "../include/asm.h"
 
-
-int		main(void)
+void	print_error(int error)
 {
+	if (error == 1)
+		ft_putstr("Usage: ./asm <sourcefile.s>\n"); 
+	else if (error == 2)
+		ft_putstr("Can not read source file \n");
+	exit (0);
+}
+
+
+int		main(int argc, char **argv)
+{
+	int	i;
+
+	i = 0;
+	if (argc == 1)
+		print_error(1);
+	else
+	{
+		while (++i < argc)
+		{
+			ft_putstr(argv[i]);
+			ft_putstr(": ");
+			if (ft_strlen(argv[i]) < 3 || argv[i][ft_strlen(argv[i]) - 2] != '.' ||
+				argv[i][ft_strlen(argv[i]) - 1] != 's')
+			print_error(2);
+		}
+	}
+	
 	return (0);
 }
